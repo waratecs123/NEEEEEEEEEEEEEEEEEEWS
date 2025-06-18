5881ab427d31ae02fbf9ce89df369c80517bc34a
Name <netuimanetuima@gmail.com>
2025-06-18 20:04:23 +1000

    ffffffffff

---
+++ project/simpleapp/views.py
@@ -0,0 +1,29 @@
+from datetime import datetime
+from lib2to3.fixes.fix_input import context
+
+from django.views.generic import ListView, DetailView
+from .models import Product
+
+
+class ProductsList(ListView):
+    # Указываем модель, объекты которой мы будем выводить
+    model = Product
+    # Поле, которое будет использоваться для сортировки объектов
+    ordering = 'name'
+    # Указываем имя шаблона, в котором будут все инструкции о том,
+    # как именно пользователю должны быть показаны наши объекты
+    template_name = 'products.html'
+    # Это имя списка, в котором будут лежать все объекты.
+    # Его надо указать, чтобы обратиться к списку объектов в html-шаблоне.
+    context_object_name = 'products'
+
+class ProductDetail(DetailView):
+    model = Product
+    template_name = 'product.html'
+    context_object_name = 'product'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['time_now'] = datetime.utcnow()
+        context['next_sale'] = None
+        return context
