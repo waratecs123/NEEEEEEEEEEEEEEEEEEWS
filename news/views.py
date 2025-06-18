b7e738cebb9dfcf2edfac135e5d44cf3777514de
Name <netuimanetuima@gmail.com>
2025-06-18 18:39:47 +1000

    Ваше сообщение коммита

---
+++ news/views.py
@@ -0,0 +1,19 @@
+from django.views.generic import ListView, DetailView
+from .models import Post
+
+
+class NewsList(ListView):
+    model = Post
+    queryset = Post.objects.filter(news_choice_article="NE").order_by('-created_at')
+    template_name = 'news.html'
+    context_object_name = 'news'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        print("News in context:", context['news'])  # Проверьте, что здесь выводятся ожидаемые данные
+        return context
+
+class NewsDetail(DetailView):
+    model = Post
+    template_name = 'news_1.html'
+    context_object_name = 'post'
